95d8f5c935220eefe396aba6616bc5bf30acbf5a
dhamu-m <156929705+dhamu-m@users.noreply.github.com>
2025-06-26 17:59:23 +0530

    Add files via upload

---
+++ frontend/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const About = () => {
+  return <div><h1>ℹ️ About This App</h1></div>;
+};
+
+export default About;
+
---
+++ frontend/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div><h1>🏠 Welcome to AffordMed</h1></div>;
+};
+
+export default Home;
